abc6c315024d3231fee88fd0e245f7b5abb8efe0
Prajwal-B05 <prajwal.is20@bmsce.ac.in>
2023-06-20 16:38:45 +0530

    Udated the Pay() function

--- app.js
+++ app.js
@@ -17,7 +17,26 @@ function updateCart() {
 }
 
 function pay() {
+  console.log(JSON.stringify(cartItems));
+  let NFTP = document.querySelector('.grid')
+  const NFT1C = document.querySelector('.NFT1');
+  const NFT2C = document.querySelector('.NFT2')
+  const NFT3C = document.querySelector('.NFT3')
   
-  cartItems = [];
-  updateCart();
+  for (var i = 0; i < cartItems.length; i++) {
+    if (cartItems[i] === "NFT1") {
+      var divele = document.querySelector('.NFT1');
+      NFTP.removeChild(divele);
+    } else if (cartItems[i] === "NFT2") {
+      var divele = document.querySelector('.NFT2');
+      NFTP.removeChild(divele);
+    } else if (cartItems[i] === "NFT3") {
+      var divele = document.querySelector('.NFT3');
+      NFTP.removeChild(divele);
+    }
+  }
+cartItems = [];
+updateCart();
+console.log(JSON.stringify(cartItems));
+
 }
